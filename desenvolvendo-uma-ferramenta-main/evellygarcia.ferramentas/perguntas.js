a912bffed40c78f4f25fcac894072408bde75dd5
Evellyg <161784120+Evellyg@users.noreply.github.com>
2024-11-14 22:19:39 -0300

    Add files via upload

---
+++ desenvolvendo-uma-ferramenta-main/evellygarcia.ferramentas/perguntas.js
@@ -0,0 +1,43 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)', 
+) 
+
+criaCartao(
+    'Qual lingua e falado o verbo?',
+    'o portugues',
+) 
+
+criaCartao(
+    'O sol e oque no sistema solar?',
+    'Uma estrla',
+) 
+
+criaCartao(
+    'Quantos dias dura a gravidez de um canguru?',
+    'cerca de 200 dias',    
+) 
+
+criaCartao(
+    'Quantos anos jesus tinha?',
+    'cerca de 30',    
+) 
